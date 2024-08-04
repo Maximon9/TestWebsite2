dac7b96a7f17376ab3831ba4fba8d8078b586403
Max <aamaximon989@gmail.com>
2024-08-04 05:04:53 -0500

    update 6 files and delete 1 file

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,2 @@
-import { createServer } from "http2";
-const host = "localhost";
-const port = 8000;
-const requestListener = function (req, res) {
-    res.writeHead(200);
-    res.end("My first server!");
-};
-const server = createServer(requestListener);
-server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`);
-});
+export {};
 //# sourceMappingURL=index.js.map
